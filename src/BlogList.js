04a6ac0ae43fcda34ecd36bfd7b0f82c1d50d322
trevor.m <mutindatrevor@gmail.com>
2024-03-13 16:56:10 +0300

    added props

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+// const BlogList = (props) => {
+//   const blogs = props.blogs;
+//   const title = props.title;
+
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((e) => (
+        <div className="blog-preview" key={e.id}>
+          <h2>{e.title}</h2>
+          <p>Written by {e.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My First Blog",
+      body: "lorem ipsum...",
+      author: "placeboUser1",
+      id: 1,
+    },
+    {
+      title: "Hello World!",
+      body: "lorem ipsum...",
+      author: "placeboUser1",
+      id: 2,
+    },
+    {
+      title: "My Dev Journey",
+      body: "lorem ipsum...",
+      author: "placeboUser",
+      id: 3,
+    },
+  ]);
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs:" />
+      <BlogList
+        blogs={blogs.filter((auth) => auth.author === "placeboUser1")}
+        title="Author Blogs:"
+      />
+    </div>
+  );
+};
+
+export default Home;
