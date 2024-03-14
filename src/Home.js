598d086961a09205f264ab23b2322aea031ef2f9
trevor.m <mutindatrevor@gmail.com>
2024-03-14 14:03:24 +0300

    use state hook

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+
+// Use State Hook
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -22,15 +24,20 @@ const Home = () => {
       id: 3,
     },
   ]);
-
+  const [name, setName] = useState("Me");
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((e) => e.id !== id);
     setBlogs(newBlogs);
   };
+  useEffect(() => {
+    console.log("Use Effect dry-ran");
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs:" handleDelete={handleDelete} />
+      <button onClick={() => setName("Them")}>Change Name</button>
+      <p>{name}</p>
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "placeboUser1")}
         title="First Author's Blogs:"
