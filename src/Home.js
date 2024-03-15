10677adb077a1259fec12490361e9acbc3161d4d
trevor.m <mutindatrevor@gmail.com>
2024-03-15 15:46:28 +0300

    fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -4,40 +4,38 @@ import BlogList from "./BlogList";
 // Use State Hook
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My First Blog",
-      body: "lorem ipsum...",
-      author: "placeboUser1",
-      id: 1,
-    },
-    {
-      title: "Hello World!",
-      body: "lorem ipsum...",
-      author: "placeboUser1",
-      id: 2,
-    },
-    {
-      title: "My Dev Journey",
-      body: "lorem ipsum...",
-      author: "placeboUser",
-      id: 3,
-    },
-  ]);
-  const [name, setName] = useState("Me");
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((e) => e.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
+  // const [name, setName] = useState("Me");
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((e) => e.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+
+  // useEffect(() => {
+  //   console.log("Use Effect dry-ran");
+  // }, [name]);
   useEffect(() => {
-    console.log("Use Effect dry-ran");
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs:" handleDelete={handleDelete} />
-      <button onClick={() => setName("Them")}>Change Name</button>
-      <p>{name}</p>
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs:"
+          // handleDelete={handleDelete}
+        />
+      )}
+      {/* <button onClick={() => setName("Them")}>Change Name</button>
+      <p>{name}</p> */}
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "placeboUser1")}
         title="First Author's Blogs:"
