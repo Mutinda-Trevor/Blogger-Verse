1486b37858673c1c85ca708d4b799e10b52a454b
trevor.m <mutindatrevor@gmail.com>
2024-03-16 22:51:15 +0300

    Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 // Use State Hook
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   // const [name, setName] = useState("Me");
 
@@ -18,27 +21,6 @@ const Home = () => {
   // useEffect(() => {
   //   console.log("Use Effect dry-ran");
   // }, [name]);
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        console.log(res);
-
-        if (!res.ok) {
-          throw Error("Could not fetch the data from the resource");
-        }
-        return res.json();
-      })
-
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
-      });
-  }, []);
 
   return (
     <div className="home">
