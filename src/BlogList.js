9ff1607d26c52d829b3ba6b12a5344e8d5251d9e
trevor.m <mutindatrevor@gmail.com>
2024-03-20 16:31:08 +0300

    Route Parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,15 +2,19 @@
 //   const blogs = props.blogs;
 //   const title = props.title;
 
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({ blogs, title /*handleDelete*/ }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((e) => (
         <div className="blog-preview" key={e.id}>
-          <h2>{e.title}</h2>
-          <p>Written by {e.author}</p>
-          {/* <button onClick={() => handleDelete(e.id)}>Delete Block</button> */}
+          <Link to={`/blogs/${e.id}`}>
+            <h2>{e.title}</h2>
+            <p>Written by {e.author}</p>
+            {/* <button onClick={() => handleDelete(e.id)}>Delete Block</button> */}
+          </Link>
         </div>
       ))}
     </div>
