4bc21bca145e33ad78d3323d6179c824b2f8047a
trevor.m <mutindatrevor@gmail.com>
2024-03-21 14:51:02 +0300

    Added Form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState(" ");
+  const [body, setBody] = useState(" ");
+  const [author, setAuthor] = useState("PlaceboUSer1");
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}>
+          required
+        </textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => e.target.value}>
+          <option value="PlaceboUSer1">PlaceboUSer1</option>
+          <option value="PlaceboUSer2">PlaceboUSer2</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
